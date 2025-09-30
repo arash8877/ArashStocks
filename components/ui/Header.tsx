e907f748c538c709372873e29c89ed304f229d43
Arash <arash8877@gmail.com>
2025-09-30 13:54:11 +0200

    create basic header

---
+++ components/ui/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/ui/NavItems";
+
+const Header = () => {
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+                <Link href='/public'>
+                    <Image src="/assets/icons/logo.svg" alt="logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
+                </Link>
+                <nav className="hidden sm:block">
+                    <NavItems/>
+                </nav>
+            </div>
+        </header>
+    )
+}
+export default Header
